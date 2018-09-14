79f658d0875d84e5e4243ca9dd62bee271e48d44
Scifsyh <vjlad9520@gmail.com>
2018-09-14 09:08:46 +0300

    Solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var answer = [];
+  var key = /([+-]?[^^]\s?\d+)/g;
+  equation = equation.replace(/\s+/g, '');
+  var result = equation.match(key);
+  return calculate(result[0], result[1], result[2]);
+}
+
+function calculate(a, b, c) {
+  var d = Math.pow(b, 2) - 4*a*c;
+  var x1 = (-b + Math.sqrt(d, 0.5))/(2*a);
+  var x2 = (-b - Math.sqrt(d, 0.5))/(2*a);
+  return [Math.round(x1), Math.round(x2)].sort((a, b) => a - b);
 }
